29b876136fbb3276ec006a4b0b27493bb229c559
James Tomasino <james@tomasino.org>
2019-12-03 17:18:14 +0000

    no more linting errors

--- 03/solution/bounds.adb
+++ 03/solution/bounds.adb
@@ -2,6 +2,42 @@ package body Bounds is
   procedure Get_Bounds (Path: in Path_Array; Max_R: out Integer; Max_L: out Integer; Max_U: out Integer; Max_D: out Integer) is
     Current_X : Integer := 0;
     Current_Y : Integer := 0;
+    Direction : String(1..7);
+    H : Character;
+    T : Integer;
+
+    procedure Update_R (T: Integer) is
+    begin
+      Current_X := Current_X + T;
+      if Current_X > Max_R then
+        Max_R := Current_X;
+      end if;
+    end Update_R;
+
+    procedure Update_L (T: Integer) is
+    begin
+      Current_X := Current_X - T;
+      if Current_X < Max_L then
+        Max_L := Current_X;
+      end if;
+    end Update_L;
+
+    procedure Update_U (T: Integer) is
+    begin
+      Current_Y := Current_Y + T;
+      if Current_Y > Max_U then
+        Max_U := Current_Y;
+      end if;
+    end Update_U;
+
+    procedure Update_D (T: Integer) is
+    begin
+      Current_Y := Current_Y - T;
+      if Current_Y < Max_D then
+        Max_D := Current_Y;
+      end if;
+    end Update_D;
+
   begin
     -- These are placeholders so the 'out' doesn't get annoyed with me
     Max_R := 1;
@@ -11,14 +47,17 @@ package body Bounds is
 
     -- Loop over the path array and check each direction to see which way we're moving
     for I in Path'Range loop
-      case Path(I)'First is
+      Direction := Path(I);
+      H := Direction(1);
+      T := Integer'Value(Direction(2..7));
+      case H is
         -- Get the first character to indicate direction.
         -- Look at the remaining chars as integers and move "current" that way
         -- If we exceed the bounds of a Max, update it.
-        when 'R'    => exit; -- none of these work because it "found a character type"
-        when 'L'    => exit; -- none of these work because it "found a character type"
-        when 'U'    => exit; -- none of these work because it "found a character type"
-        when 'D'    => exit; -- none of these work because it "found a character type"
+        when 'R'    => Update_R(T);
+        when 'L'    => Update_L(T);
+        when 'U'    => Update_U(T);
+        when 'D'    => Update_D(T);
         when others => -- handle these later
           null;
       end case;
--- 03/solution/bounds.ads
+++ 03/solution/bounds.ads
@@ -1,5 +1,4 @@
 package Bounds is
-  type String_Access is not null access constant String;
-  type Path_Array is array (positive range <>) of String_Access;
+  type Path_Array is array (positive range <>) of String(1..7);
   procedure Get_Bounds (Path: in Path_Array; Max_R: out Integer; Max_L: out Integer; Max_U: out Integer; Max_D: out Integer);
 end Bounds;
